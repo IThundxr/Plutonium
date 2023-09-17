93bfed8847f524896ef9c73433aaa21ffaeb8897
IThundxr <contact@ithundxr.dev>
2023-09-16 21:56:40 -0400

    rename class

--- src/main/java/dev/ithundxr/plutonium/mixin/PlutoniumMixinPlugin.java
+++ src/main/java/dev/ithundxr/plutonium/mixin/PlutoniumMixinPlugin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import java.util.List;
 import java.util.Set;
 
-public class PlutoniumMixin implements IMixinConfigPlugin {
+public class PlutoniumMixinPlugin implements IMixinConfigPlugin {
     @Override
     public void onLoad(String s) {}
 
